3bf3641ec0338404a33ae0e889b17393d3588717
rutgerhofstee <42>
2017-07-24 19:25:56 +0000

    $message

---
+++ notebooks/Untitled.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# In[1]:
+
+get_ipython().system('echo $message')
+
+
+# In[2]:
+
+get_ipython().system("export message='custom'")
+
+
+# In[3]:
+
+get_ipython().system('echo $message')
+
+
+# In[ ]:
+
+
+
