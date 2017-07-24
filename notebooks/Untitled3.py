2880d7037aaedb09e7a9dc2324585acd7adfc5da
rutgerhofstee <42>
2017-07-24 20:04:25 +0000

    created new notebook2

---
+++ notebooks/Untitled3.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[1]:
+
+from IPython.display import IFrame
+IFrame(src="https://athena.brynmawr.edu/jupyter/user/dblank/terminals/1", width="100%", height="400px")
+
+
+# In[ ]:
+
+
+
