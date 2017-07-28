d46f895049c578be8d1408297e551e90f06246cf
rutgerhofste <rutgerhofste@gmail.com>
2017-07-28 19:58:45 +0000

    Saved in Jupyter (browser)

--- notebooks/other/Y2017M07D28_RH_python27setup_v01.py
+++ notebooks/other/Y2017M07D28_RH_python27setup_v01.py
@@ -10,4 +10,32 @@
 # 
 # 
 
-#  
+# In[ ]:
+
+packages = {""}
+
+
+# In[2]:
+
+
+
+try:
+    from osgeo import gdal
+    gdal =1
+except:
+    gdal = 0
+    
+    
+    
+    
+
+
+# In[3]:
+
+print(gdal)
+
+
+# In[ ]:
+
+
+
