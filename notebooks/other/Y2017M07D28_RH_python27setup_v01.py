2f374a9d5b431e9072dce5bab0aa8a7f92636f73
rutgerhofste <rutgerhofste@gmail.com>
2017-07-28 19:55:46 +0000

    Saved in Jupyter (browser)

---
+++ notebooks/other/Y2017M07D28_RH_python27setup_v01.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# # Title
+# 
+# * Purpose of script: test python 27 environement against several libraries  
+# * Author: Rutger Hofste
+# * Kernel used: python27
+# * Date created: 20170728
+# 
+# 
+
+# In[ ]:
+
+
+
---
+++ notebooks/other/python27testSetup.py
@@ -0,0 +1,11 @@
+
+# coding: utf-8
+
+# # Title
+# 
+# Purpose of script: test python 27 environement against several libraries
+# Author: Rutger Hofste
+# Kernel used: python27
+# Date created: 20170728
+# 
+# 
