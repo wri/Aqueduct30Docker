c5da9c8ee7d9bcdba7b3276565e5598e5470db21
rutgerhofste <rutgerhofste@gmail.com>
2017-09-01 22:12:01 +0000

    restructured average supply notebook

--- notebooks/production/Y2017M08D30_RH_Average_Supply_EE_V01.py
+++ notebooks/production/Y2017M08D30_RH_Average_Supply_EE_V01.py
@@ -25,55 +25,66 @@ ee.Initialize()
 # In[3]:
 
 EE_INPUT_PATH = "projects/WRI-Aquaduct/PCRGlobWB20V07/"
+
+EE_IC_NAME_ANNUAL =  "global_historical_reducedmeanrunoff_year_myear_5min_1960_2014"
+EE_IC_NAME_MONTH =   "global_historical_reducedmeanrunoff_month_mmonth_5min_1960_2014"
+
+EE_I_NAME_ANNUAL = EE_IC_NAME_ANNUAL
+EE_I_NAME_MONTH = EE_IC_NAME_MONTH
+
+
 INPUT_FILE_NAME_ANNUAL = "global_historical_runoff_year_myear_5min_1958_2014"
 INPUT_FILE_NAME_MONTH = "global_historical_runoff_month_mmonth_5min_1958_2014"
+
 YEAR_MIN = 1960
 YEAR_MAX = 2014
 ANNUAL_UNITS = "m/year"
 MONTHLY_UNITS = "m/month"
-ANNUAL_EXPORTDESCRIPTION = "runoff_annua_myear_reduced"
-MONTHLY_EXPORTDESCRIPTION = "runoff_month_mmonth_reduced"
-VERSION = "30"
+ANNUAL_EXPORTDESCRIPTION = "reducedmeanrunoff_year" #final format reducedmeanrunoff_yearY1960Y2014
+MONTHLY_EXPORTDESCRIPTION = "reducedmeanrunoff_month" #final format reducedmeanrunoff_monthY1960Y2014M01
+VERSION = "33"
 
+
+# The Standardized format to store assets on Earth Engine is EE_INPUT_PATH / EE_IC_NAME / EE_I_NAME and every image should have the property expertdescription that would allow to export the data to a table header. 
 
 # In[4]:
 
-def exportToAssetMonth(month):
-    scale = ee.Image(icMonthly.first()).projection().nominalScale().getInfo()
-    monthFilter = ee.Filter.eq("month",ee.Number(month))
-    monthlyImage = filteredMonthlyCollection.reduce(ee.Reducer.mean())
-    monthlyImage  = monthlyImage.set("month",ee.Number(month)).set("rangeMin",ee.Number(YEAR_MIN)).set("rangeMax",ee.Number(YEAR_MAX)).set("reducer",ee.String("mean")).set("units","m/month")
-    exportdescription = MONTHLY_EXPORTDESCRIPTION + "_M%s" %(month)
-    monthlyImage  = monthlyImage.set("exportdescription",exportdescription)
-    monthlyImage  = monthlyImage.set("creation","RutgerHofste_20170901_Python27") 
+def exportToAssetAnnual(ic):
+    annualExportDescription = ANNUAL_EXPORTDESCRIPTION + "Y%sY%s" %(YEAR_MIN,YEAR_MAX)
+    annualImage = ee.Image(ic.reduce(ee.Reducer.mean()))
+    annualImage = annualImage.set("rangeMin",ee.Number(YEAR_MIN)).set("rangeMax",ee.Number(YEAR_MAX)).set("units",ANNUAL_UNITS).set("exportdescription",annualExportDescription).set("creation","RutgerHofste_20170901_Python27")
+    scale = ee.Image(icAnnual.first()).projection().nominalScale().getInfo()
+    assetId = EE_INPUT_PATH + EE_IC_NAME_ANNUAL + "V" +VERSION+ "/" + EE_I_NAME_ANNUAL + "V" + VERSION    
+    task = ee.batch.Export.image.toAsset(
+        image =  ee.Image(annualImage),
+        description = EE_I_NAME_ANNUAL + "V" + VERSION,
+        assetId = assetId,
+        scale = scale,
+        region = geometry.bounds().getInfo()['coordinates'][0],
+        maxPixels = 1e10
+    )
+    task.start()
+    return 1
 
+
+def exportToAssetMonth(month):
+    monthlyExportDescription = MONTHLY_EXPORTDESCRIPTION + "Y%sY%sM%0.2d" %(YEAR_MIN,YEAR_MAX,month)
+    monthlyImage = filteredMonthlyCollection.filter(ee.Filter.eq("month",ee.Number(month)))
+    monthlyImage = filteredMonthlyCollection.reduce(ee.Reducer.mean())
+    monthlyImage  = monthlyImage.set("month",ee.Number(month)).set("rangeMin",ee.Number(YEAR_MIN)).set("rangeMax",ee.Number(YEAR_MAX)).set("reducer",ee.String("mean")).set("units",MONTHLY_UNITS).set("exportdescription",monthlyExportDescription).set("creation","RutgerHofste_20170901_Python27")
+    scale = ee.Image(icMonthly.first()).projection().nominalScale().getInfo()
+
+    assetId = EE_INPUT_PATH + EE_IC_NAME_MONTH + "V" +VERSION+ "/" + EE_I_NAME_MONTH + "M%0.2dV%s" %(month,VERSION)  
     task = ee.batch.Export.image.toAsset(
         image =  ee.Image(monthlyImage),
-        description = exportdescription,
-        assetId = icMonthPath + "/" + exportdescription,
+        description = EE_I_NAME_MONTH + "M%0.2dV%s" %(month,VERSION) ,
+        assetId = assetId,
         scale = scale,
         region = geometry.bounds().getInfo()['coordinates'][0],
         maxPixels = 1e10
     )
     task.start()
     print(month)
-    return 1
-
-
-def exportToAssetAnnual(ic):
-    annualImage = ee.Image(ic.reduce(ee.Reducer.mean()))
-    annualImage = annualImage.set("rangeMin",ee.Number(YEAR_MIN)).set("rangeMax",ee.Number(YEAR_MAX)).set("units",ANNUAL_UNITS).set("exportdescription",ANNUAL_EXPORTDESCRIPTION).set("creation","RutgerHofste_20170901_Python27")
-    scale = ee.Image(icAnnual.first()).projection().nominalScale().getInfo()
-    task = ee.batch.Export.image.toAsset(
-        image =  ee.Image(annualImage),
-        description = ANNUAL_EXPORTDESCRIPTION,
-        assetId = EE_INPUT_PATH + ANNUAL_EXPORTDESCRIPTION,
-        scale = scale,
-        region = geometry.bounds().getInfo()['coordinates'][0],
-        maxPixels = 1e10
-    )
-    task.start()
-    print("done")
     return 1
     
     
@@ -104,8 +115,8 @@ filteredMonthlyCollection = ee.ImageCollection(icMonthly.filter(dateFilterMin).f
 
 # In[9]:
 
-icMonthPath = os.path.join(EE_INPUT_PATH,MONTHLY_EXPORTDESCRIPTION+ "V" + VERSION)
-command = ("earthengine create collection %s") %icMonthPath
+icAnnualPath = os.path.join(EE_INPUT_PATH,EE_IC_NAME_ANNUAL+ "V" + VERSION)
+command = ("earthengine create collection %s") %icAnnualPath
 print(command)
 
 
@@ -116,17 +127,34 @@ subprocess.check_output(command,shell=True)
 
 # In[11]:
 
-months = list(range(1,13))
+icMonthPath = os.path.join(EE_INPUT_PATH,EE_IC_NAME_MONTH+ "V" + VERSION)
+command = ("earthengine create collection %s") %icMonthPath
+print(command)
+
+
+# In[12]:
+
+subprocess.check_output(command,shell=True)
+
+
+# In[ ]:
+
+
 
 
 # ### Run the functions
 
-# In[12]:
+# In[13]:
 
 exportToAssetAnnual(filteredAnnualCollection)
 
 
-# In[13]:
+# In[14]:
+
+months = list(range(1,13))
+
+
+# In[15]:
 
 map(exportToAssetMonth,months)
 
--- notebooks/production/Y2017M09D01_RH_linear_trend_Ag_Demand_EE_V01.py
+++ notebooks/production/Y2017M09D01_RH_linear_trend_Ag_Demand_EE_V01.py
@@ -8,7 +8,7 @@
 # * Kernel used: python27
 # * Date created: 20170901
 
-# In[9]:
+# In[1]:
 
 import os
 import ee
@@ -19,57 +19,55 @@ import subprocess
 
 # Settings:
 
-# In[25]:
+# In[48]:
 
 EE_INPUT_PATH = "projects/WRI-Aquaduct/PCRGlobWB20V07/"
 INPUT_FILE_NAME_WW_ANNUAL = "global_historical_PIrrWW_year_millionm3_5min_1960_2014"
 INPUT_FILE_NAME_WN_ANNUAL = "global_historical_PIrrWN_year_millionm3_5min_1960_2014"
-INPUT_FILE_NAME_WW_MONTHLY = "global_historical_PIrrWW_month_millionm3_5min_1960_2014"
-INPUT_FILE_NAME_WN_MONTHLY = "global_historical_PIrrWN_month_millionm3_5min_1960_2014"
+INPUT_FILE_NAME_WW_MONTH = "global_historical_PIrrWW_month_millionm3_5min_1960_2014"
+INPUT_FILE_NAME_WN_MONTH = "global_historical_PIrrWN_month_millionm3_5min_1960_2014"
 YEAR_MIN = 2004
 YEAR_MAX = 2014
 
+FILE_NAME_MONTH_WW = "global_historical_PIrrWWlinear_month_millionm3_5min_2004_2014"
+FILE_NAME_MONTH_WN = "global_historical_PIrrWNlinear_month_millionm3_5min_2004_2014"
 
-# In[26]:
+
+
+# In[3]:
 
 ee.Initialize()
 
 
-# In[27]:
+# In[4]:
 
 propertiesWWannua = {"units":"millionm3","parameter":"IrrWWlinear_year","year":2014,"month":12,"exportdescription":"IrrWWLinear_annuaY2014M12"}
 propertiesWNannua = {"units":"millionm3","parameter":"IrrWNlinear_year","year":2014,"month":12,"exportdescription":"IrrWNLinear_annuaY2014M12"}
 
 
-# In[28]:
+# In[5]:
 
 geometry = ee.Geometry.Polygon(coords=[[-180.0, -90.0], [180,  -90.0], [180, 89], [180,90]], proj= ee.Projection('EPSG:4326'),geodesic=False )
 
 
-# In[29]:
+# In[6]:
 
 print(os.path.join(EE_INPUT_PATH,INPUT_FILE_NAME_WW_ANNUAL))
 
 
-# In[30]:
+# In[7]:
 
 icWWannua = ee.ImageCollection(os.path.join(EE_INPUT_PATH,INPUT_FILE_NAME_WW_ANNUAL));
 icWNannua = ee.ImageCollection(os.path.join(EE_INPUT_PATH,INPUT_FILE_NAME_WN_ANNUAL));
 
 
-# In[32]:
+# In[8]:
 
 irrWW2014 = ee.Image(icWWannua.filter(ee.Filter.calendarRange(YEAR_MIN,YEAR_MAX,"year")).first())
 irrWN2014 = ee.Image(icWNannua.filter(ee.Filter.calendarRange(YEAR_MIN,YEAR_MAX,"year")).first())
 
 
-# In[38]:
-
-icWWRange = icWWannua.filter(ee.Filter.calendarRange(YEAR_MIN,YEAR_MAX,"year"))
-icWNRange = icWNannua.filter(ee.Filter.calendarRange(YEAR_MIN,YEAR_MAX,"year"))
-
-
-# In[39]:
+# In[37]:
 
 def createTimeBand(image):
     # Adds a timeband to the single band image. band is "b1" 
@@ -79,36 +77,115 @@ def createTimeBand(image):
     return image.addBands(newImage)   
     
     
-def linearTrendAnnual(ic,yearmin,yearmax):
-    ic_filtered = ic.filter(ee.Filter.calendarRange(YEAR_MIN,YEAR_MAX,"year"))
+def linearTrendAnnual(ic,yearmin,yearmax,outputFileName,units,exportdescription):
+    nominalScale = ee.Image(ic.first()).projection().nominalScale().getInfo()
+    icFiltered = ic.filter(ee.Filter.calendarRange(YEAR_MIN,YEAR_MAX,"year"))
+    icFilteredTimeband = icFiltered.map(createTimeBand)
     imageFinalYear = ee.Image(ic.filter(ee.Filter.calendarRange(YEAR_MIN,YEAR_MAX,"year")).first())
+    fit = icFilteredTimeband.select(["independent","dependent"]).reduce(ee.Reducer.linearFit())
+    offset = fit.select(["offset"])
+    scale = fit.select(["scale"]) #Note that this definition of scale is a as in ax+b
+    newImageYearMax = scale.multiply(YEAR_MAX).add(offset).select(["scale"],["newValue"])
+    spatialScale = imageFinalYear.projection().nominalScale()
+    exportImageToAsset(newImageYearMax,outputFileName,units,exportdescription,nominalScale)
+    return newImageYearMax
+
+
+def exportImageToAsset(image,outputFileName,units,exportdescription,scale):
+    image = image.set("rangeMin",ee.Number(YEAR_MIN)).set("rangeMax",ee.Number(YEAR_MAX)).set("units",units).set("exportdescription",exportdescription).set("creation","RutgerHofste_20170901_Python27")
+    task = ee.batch.Export.image.toAsset(
+        image =  ee.Image(image),
+        description = outputFileName,
+        assetId = EE_INPUT_PATH + outputFileName,
+        scale = scale,
+        region = geometry.bounds().getInfo()['coordinates'][0],
+        maxPixels = 1e10
+    )
+    task.start()
+    
+def iterateMonths(month):
+    # parameters defined prior to running function!
+    linearTrendMonth(ic,yearmin,yearmax,outputFileName,units,exportdescription,month)
     
     
+    
+def linearTrendMonth(ic,yearmin,yearmax,outputFileName,units,exportdescription,month):
+    nominalScale = ee.Image(ic.first()).projection().nominalScale().getInfo()
+    icFiltered = ic.filter(ee.Filter.calendarRange(YEAR_MIN,YEAR_MAX,"year"))
+    icFiltered = icFiltered.filter(ee.Filter.eq("month",ee.Number(month)))
+    icFilteredTimeband = icFiltered.map(createTimeBand)
+    imageFinalYear = ee.Image(ic.filter(ee.Filter.calendarRange(YEAR_MIN,YEAR_MAX,"year")).first())
+    fit = icFilteredTimeband.select(["independent","dependent"]).reduce(ee.Reducer.linearFit())
+    offset = fit.select(["offset"])
+    scale = fit.select(["scale"]) #Note that this definition of scale is a as in ax+b
+    newImageYearMax = scale.multiply(YEAR_MAX).add(offset).select(["scale"],["newValue"])
+    spatialScale = imageFinalYear.projection().nominalScale()
+    exportImageToAsset(newImageYearMax,outputFileName,units,exportdescription,nominalScale)
+    return newImageYearMax
+
+
+# Usage: (ic,yearmin,yearmax,outputFileName,units,exportdescription)
+
+# In[39]:
+
+newImage2014WW = linearTrendAnnual(icWWannua,YEAR_MIN,YEAR_MAX,"global_historical_PIrrWWlinear_year_millionm3_5min_2004_2014","millionm3","IrrWWLinear_yearY2014M12")
 
 
 # In[40]:
 
-icWWRange2 = icWWRange.map(createTimeBand)
-icWNRange2 = icWNRange.map(createTimeBand)
+newImage2014WN = linearTrendAnnual(icWNannua,YEAR_MIN,YEAR_MAX,"global_historical_PIrrWNlinear_year_millionm3_5min_2004_2014","millionm3","IrrWNLinear_yearY2014M12")
 
 
-# In[41]:
-
-fitWW = icWWRange2.select(["independent","dependent"]).reduce(ee.Reducer.linearFit())
-
-
-# In[42]:
-
-fitWN = icWNRange2.select(["independent","dependent"]).reduce(ee.Reducer.linearFit())
-
+# The monthly results should be stored in imageCollections instead of single images. Creating them first
 
 # In[43]:
 
-offsetWW = fitWW.select(["offset"])
-scaleWW = fitWW.select(["scale"])
+icMonthPathWW = os.path.join(EE_INPUT_PATH,FILE_NAME_MONTH_WW)
+command = ("earthengine create collection %s") %icMonthPathWW
+print(command)
+
+
+# In[44]:
+
+subprocess.check_output(command,shell=True)
+
+
+# In[45]:
+
+icMonthPathWN = os.path.join(EE_INPUT_PATH,FILE_NAME_MONTH_WN)
+command = ("earthengine create collection %s") %icMonthPathWN
+print(command)
+
+
+# In[46]:
+
+subprocess.check_output(command,shell=True)
+
+
+# In[47]:
+
+months = list(range(1,13))
+
+
+# In[49]:
+
+icWWmonth = ee.ImageCollection(os.path.join(EE_INPUT_PATH,INPUT_FILE_NAME_WW_MONTH));
+icWNmonth = ee.ImageCollection(os.path.join(EE_INPUT_PATH,INPUT_FILE_NAME_WN_MONTH));
+
+
+# ## Define all parameters prior to running the mapping function
+
+# In[52]:
+
+ic = icWWmonth
+yearmin = YEAR_MIN
+yearmax = YEAR_MAX
+outputFileName = "global_historical_PIrrWWlinear_month_millionm3_5min_2004_2014"
+units = "millionm3"
+exportdescription = "IrrWWLinear_monthY2014"
 
 
 # In[ ]:
 
-
+map()
 
