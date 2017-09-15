8606d6452859f994b1a36d76cd334986428b2694
rutgerhofste <rutgerhofste@gmail.com>
2017-09-15 21:17:32 +0000

    temporary save

--- notebooks/testing/Y2017M09D15_RH_fix_geometry_V01.py
+++ notebooks/testing/Y2017M09D15_RH_fix_geometry_V01.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[25]:
+# In[1]:
 
 EC2_INPUT_PATH = "/volumes/data/Y2017M09D15_RH_fix_geometry_V01/input/"
 EC2_OUTPUT_PATH = "/volumes/data/Y2017M09D15_RH_fix_geometry_V01/output/"
@@ -10,37 +10,42 @@ S3_INPUT_PATH = "s3://wri-projects/Aqueduct30/processData/Y2017M08D29_RH_Merge_F
 S3_OUTPUT_PATH = "s3://wri-projects/Aqueduct30/processData/Y2017M09D15_RH_fix_geometry_V01/output/"
 
 INPUT_FILE_NAME = "hybas_lev06_v1c_merged_fiona_upstream_downstream_FAO_V01.shp"
-OUTPUT_FILE_NAME = "hybas_lev06_v1c_merged_fiona_upstream_downstream_FAO_ogr_V01.shp"
+OUTPUT_FILE_NAME = "hybas_lev06_v1c_merged_fiona_upstream_downstream_FAO_ogr_V01.gml"
 
 
-# In[26]:
+# In[2]:
 
 get_ipython().system('rm -r {EC2_INPUT_PATH}')
 get_ipython().system('rm -r {EC2_OUTPUT_PATH}')
 
 
-# In[27]:
+# In[3]:
 
 get_ipython().system('mkdir -p {EC2_INPUT_PATH}')
 get_ipython().system('mkdir -p {EC2_OUTPUT_PATH}')
 
 
-# In[28]:
+# In[4]:
 
 get_ipython().system('aws s3 cp {S3_INPUT_PATH} {EC2_INPUT_PATH} --recursive')
 
 
-# In[31]:
+# In[ ]:
 
-command = 'ogr2ogr -f "ESRI Shapefile" %s%s %s%s -nlt POLYGON' %(EC2_OUTPUT_PATH,OUTPUT_FILE_NAME,EC2_INPUT_PATH,INPUT_FILE_NAME)
+command = "time ogr2ogr -f "PostgreSQL" PG:"host=localhost user=gis dbname=gis password=gis" wdpa.shp hybas_lev06_v1c_merged_fiona_upstream_downstream_FAO_ogr_V01 hybas_lev06_v1c_merged_fiona_upstream_downstream_FAO_V01 -nln hybas6"
 
 
-# In[32]:
+# In[5]:
+
+command = 'ogr2ogr -f "GML" %s%s %s%s -nlt POLYGON' %(EC2_OUTPUT_PATH,OUTPUT_FILE_NAME,EC2_INPUT_PATH,INPUT_FILE_NAME)
+
+
+# In[6]:
 
 print(command)
 
 
-# In[33]:
+# In[8]:
 
 get_ipython().system('aws s3 cp {EC2_OUTPUT_PATH} {S3_OUTPUT_PATH} --recursive')
 
