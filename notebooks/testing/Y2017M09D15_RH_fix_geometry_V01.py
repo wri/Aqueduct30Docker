3105d1eac4066353d12e8f9c86ff161ab4b2c792
rutgerhofste <rutgerhofste@gmail.com>
2017-09-26 09:54:33 +0000

    modified notebook geometry

--- notebooks/testing/Y2017M09D15_RH_fix_geometry_V01.py
+++ notebooks/testing/Y2017M09D15_RH_fix_geometry_V01.py
@@ -10,7 +10,7 @@ S3_INPUT_PATH = "s3://wri-projects/Aqueduct30/processData/Y2017M08D29_RH_Merge_F
 S3_OUTPUT_PATH = "s3://wri-projects/Aqueduct30/processData/Y2017M09D15_RH_fix_geometry_V01/output/"
 
 INPUT_FILE_NAME = "hybas_lev06_v1c_merged_fiona_upstream_downstream_FAO_V01.shp"
-OUTPUT_FILE_NAME = "hybas_lev06_v1c_merged_fiona_upstream_downstream_FAO_ogr_V01.gml"
+OUTPUT_FILE_NAME = "hybas_lev06_v1c_merged_fiona_upstream_downstream_FAO_ogr_V01.shp"
 
 
 # In[2]:
@@ -30,22 +30,38 @@ get_ipython().system('mkdir -p {EC2_OUTPUT_PATH}')
 get_ipython().system('aws s3 cp {S3_INPUT_PATH} {EC2_INPUT_PATH} --recursive')
 
 
-# In[ ]:
-
-command = "time ogr2ogr -f "PostgreSQL" PG:"host=localhost user=gis dbname=gis password=gis" wdpa.shp hybas_lev06_v1c_merged_fiona_upstream_downstream_FAO_ogr_V01 hybas_lev06_v1c_merged_fiona_upstream_downstream_FAO_V01 -nln hybas6"
-
-
 # In[5]:
 
-command = 'ogr2ogr -f "GML" %s%s %s%s -nlt POLYGON' %(EC2_OUTPUT_PATH,OUTPUT_FILE_NAME,EC2_INPUT_PATH,INPUT_FILE_NAME)
+import os
 
 
 # In[6]:
 
+inputFileName = os.path.join(EC2_INPUT_PATH,INPUT_FILE_NAME)
+
+
+# In[7]:
+
+outputFileName = os.path.join(EC2_OUTPUT_PATH,OUTPUT_FILE_NAME)
+
+
+# In[ ]:
+
+
+
+
+# In[12]:
+
+command = 'ogr2ogr -dialect SQLITE -sql "select PFAF_ID, ST_MakeValid(Geometry) as Geometry from hybas_lev06_v1c_merged_fiona_upstream_downstream_FAO_V01" %s %s' %(outputFileName,inputFileName)
+
+
+
+# In[13]:
+
 print(command)
 
 
-# In[8]:
+# In[ ]:
 
 get_ipython().system('aws s3 cp {EC2_OUTPUT_PATH} {S3_OUTPUT_PATH} --recursive')
 
