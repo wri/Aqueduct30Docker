01d21fe4c4308b42d697a1613c81a24707cb8034
rutgerhofste <rutgerhofste@gmail.com>
2017-11-13 10:00:35 +0000

    executed scripts

--- notebooks/production/Y2017M11D10_RH_Make_Geometry_Valid_V01.py
+++ notebooks/production/Y2017M11D10_RH_Make_Geometry_Valid_V01.py
@@ -16,9 +16,9 @@
 # 
 # http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Appendix.PostgreSQL.CommonDBATasks.html#Appendix.PostgreSQL.CommonDBATasks.PostGIS
 # 
-# 
+# database is not protected by deafault. 
 
-# In[ ]:
+# In[1]:
 
 import time, datetime, sys
 dateString = time.strftime("Y%YM%mD%d")
@@ -28,7 +28,7 @@ print(dateString,timeString)
 sys.version
 
 
-# In[4]:
+# In[23]:
 
 SCRIPT_NAME = "Y2017M11D10_RH_Make_Geometry_Valid_V01"
 
@@ -47,8 +47,13 @@ OUTPUT_FILE_NAME = "Y2017M11D10_RH_Make_Geometry_Valid_V%0.2d" %(OUTPUT_VERSION)
 
 S3_OUTPUT_PATH = "s3://wri-projects/Aqueduct30/processData/Y2017M08D29_RH_Merge_FAONames_Upstream_V01/output/"
 
+# Database settings
+TABLE_NAME = "hybasvalid03"
 
-# In[ ]:
+
+
+
+# In[3]:
 
 get_ipython().system('rm -r {EC2_INPUT_PATH}')
 get_ipython().system('rm -r {EC2_OUTPUT_PATH}')
@@ -57,12 +62,12 @@ get_ipython().system('mkdir -p {EC2_INPUT_PATH}')
 get_ipython().system('mkdir -p {EC2_OUTPUT_PATH}')
 
 
-# In[ ]:
+# In[4]:
 
 get_ipython().system('aws s3 cp {S3_INPUT_PATH} {EC2_INPUT_PATH} --recursive ')
 
 
-# In[1]:
+# In[5]:
 
 from geoalchemy2 import Geometry, WKTElement
 from sqlalchemy import *
@@ -72,112 +77,122 @@ import os
 from shapely.geometry.multipolygon import MultiPolygon
 
 
-# In[20]:
+# In[6]:
 
 get_ipython().magic('matplotlib inline')
 
 
-# In[21]:
+# In[7]:
 
 gdf = gpd.read_file(os.path.join(EC2_INPUT_PATH,INPUT_FILENAME+".shp"))
 
 
-# In[22]:
+# In[8]:
 
 gdf = gdf.set_index("PFAF_ID", drop=False)
 
 
-# In[23]:
+# In[9]:
+
+gdf.head()
+
+
+# In[10]:
+
+gdf.shape
+
+
+# In[11]:
 
 gdf2 = gdf.copy()
 
 
-# In[24]:
+# In[12]:
 
 gdf2.geometry = gdf['geometry'].apply(lambda x: MultiPolygon([x]))
 
 
-# In[28]:
+# In[13]:
 
 gdf3 = gdf2.copy()
 
 
-# In[29]:
+# In[14]:
 
 gdf3['geom'] = gdf2['geometry'].apply(lambda x: WKTElement(x.wkt, srid=4326))
 
 
-# In[35]:
+# In[15]:
 
 gdf3.drop("geometry",1, inplace=True)
 
 
+# In[20]:
+
+gdf3.shape
+
+
 # The following command will connect to a temporary free tier AWS RDS instance
 
-# In[40]:
+# In[21]:
 
 engine = create_engine('postgresql://rutgerhofste:nopassword@aqueduct30v02.cgpnumwmfcqc.eu-central-1.rds.amazonaws.com:5432/database01')
 
 
-# In[41]:
+# In[26]:
 
-gdf3.to_sql("hybasvalid02", engine, if_exists='replace', index=False, 
+gdf3.to_sql(TABLE_NAME, engine, if_exists='replace', index=False, 
                          dtype={'geom': Geometry('MULTIPOLYGON', srid= 4326)})
 
 
-# In[42]:
+# In[27]:
 
 connection = engine.connect()
 
 
-# In[43]:
+# In[28]:
 
-sql = "update hybasvalid02 set geom = st_makevalid(geom)"
+sql = "update %s set geom = st_makevalid(geom)" %(TABLE_NAME)
 
 
-# In[ ]:
+# In[29]:
 
 result = connection.execute(sql)
 
 
 # Check if operation succesful 
 
-# In[ ]:
+# In[30]:
 
-sql = "select * from hybasvalid02"
-
-
-# In[ ]:
-
-gdfAWSPolygon=gpd.GeoDataFrame.from_postgis(sql,connection,geom_col='geom' ).set_index("PFAF_ID", drop=False)
+sql = "select * from %s" %(TABLE_NAME)
 
 
-# In[ ]:
+# In[31]:
 
-sql = "select * from hybasvalidmultipolygon01"
-
-
-# In[ ]:
-
-gdfAWSMultiPolygon=gpd.GeoDataFrame.from_postgis(sql,connection,geom_col='geom' ).set_index("PFAF_ID", drop=False)
+gdfAWS=gpd.GeoDataFrame.from_postgis(sql,connection,geom_col='geom' ).set_index("PFAF_ID", drop=False)
 
 
-# In[ ]:
-
-gdfAWS = gdfAWSPolygon.append(gdfMultiPolygon)
-
-
-# In[ ]:
-
-gdfAWS["valid"] = gdfAWS.geom.is_valid
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
+# In[32]:
 
 connection.close()
+
+
+# In[33]:
+
+gdfAWS.head()
+
+
+# In[ ]:
+
+gdfAWS.to_file(os.path.join(EC2_OUTPUT_PATH,OUTPUT_FILE_NAME+".shp"))
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
 
