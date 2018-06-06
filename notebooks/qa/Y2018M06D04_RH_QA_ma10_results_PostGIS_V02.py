47e5a89391a958e2add1738361ddebf7b011a143
rutgerhofste <rutgerhofste@gmail.com>
2018-06-06 16:57:18 +0000

    script to create postgis table for arc

--- notebooks/qa/Y2018M06D04_RH_QA_ma10_results_PostGIS_V02.py
+++ notebooks/qa/Y2018M06D04_RH_QA_ma10_results_PostGIS_V02.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[14]:
+# In[1]:
 
 """ QA for water stress in several basin
 -------------------------------------------------------------------------------
@@ -20,11 +20,13 @@ Docker: rutgerhofste/gisdocker:ubuntu16.04
 
 TESTING = 1
 OVERWRITE_OUTPUT = 1
-SCRIPT_NAME = 'Y2018M06D04_RH_QA_ma10_results_PostGIS_V01'
-OUTPUT_VERSION = 4
+SCRIPT_NAME = 'Y2018M06D04_RH_QA_ma10_results_PostGIS_V02'
+OUTPUT_VERSION = 6
 
 DATABASE_ENDPOINT = "aqueduct30v05.cgpnumwmfcqc.eu-central-1.rds.amazonaws.com"
 DATABASE_NAME = "database01"
+
+SIMPLIFY_COLUMN_NAMES = 1 # ForS Shapefile and ArcGIS use.
 
 INPUT_TABLE_LEFT_NAME = 'y2018m06d04_rh_water_stress_postgis_30spfaf06_v01_v03'
 INPUT_TABLE_RIGHT_NAME = 'hybas06_v04'
@@ -33,11 +35,11 @@ OUTPUT_SCHEMA = "test"
 
 
 # Filter 
-TEMPORAL_RESOLUTION = "year"
-YEAR = 2010
-MONTH = 12
-PFAFID_RANGE_MIN = 0      #158904 - 1
-PFAFID_RANGE_MAX = 999999 #172111 + 1
+TEMPORAL_RESOLUTION = None  # Option [ "year", "month"]
+YEAR_RANGE = [2010,2014] # Options [1960:2014]
+MONTH_RANGE = [1,12] # Options [1:12]
+PFAFID_RANGE = [111011,914900] # Options, list [111011:914900], includes 0 and end.
+
 
 
 print("Input Table Left: " , INPUT_TABLE_LEFT_NAME, 
@@ -46,7 +48,7 @@ print("Input Table Left: " , INPUT_TABLE_LEFT_NAME,
 
 
 
-# In[15]:
+# In[2]:
 
 import time, datetime, sys
 dateString = time.strftime("Y%YM%mD%d")
@@ -56,7 +58,7 @@ print(dateString,timeString)
 sys.version
 
 
-# In[16]:
+# In[3]:
 
 # imports
 import re
@@ -68,7 +70,7 @@ from sqlalchemy import *
 pd.set_option('display.max_columns', 500)
 
 
-# In[17]:
+# In[4]:
 
 F = open("/.password","r")
 password = F.read().splitlines()[0]
@@ -84,58 +86,118 @@ if OVERWRITE_OUTPUT:
     sqls.append("DROP TABLE IF EXISTS {}.temp_left".format(OUTPUT_SCHEMA))
 
 
-# In[18]:
+# In[5]:
+
+sql = ""
+sql += "CREATE TABLE {}.temp_left AS ".format(OUTPUT_SCHEMA)
+sql += "SELECT * FROM {} ".format(INPUT_TABLE_LEFT_NAME)
+
+if TEMPORAL_RESOLUTION or YEAR_RANGE or MONTH_RANGE or PFAFID_RANGE:
+    sql += "WHERE "
+    if TEMPORAL_RESOLUTION:
+        sql += "temporal_resolution = '{}' AND ".format(TEMPORAL_RESOLUTION)
+    if YEAR_RANGE:
+        sql += "year >= {} AND ".format(YEAR_RANGE[0])
+        sql += "year <= {} AND ".format(YEAR_RANGE[1])
+    if MONTH_RANGE:
+        sql += "month >= {} AND ".format(MONTH_RANGE[0])
+        sql += "month <= {} AND ".format(MONTH_RANGE[1])
+    if PFAFID_RANGE:
+        sql += "pfafid_30spfaf06 >= {} AND ".format(PFAFID_RANGE[0])
+        sql += "pfafid_30spfaf06 <= {} AND ".format(PFAFID_RANGE[1])
+    # remove trailing AND
+    sql = sql[:-4]
+    
+sqls.append(sql)
+
+
+# In[6]:
+
+if SIMPLIFY_COLUMN_NAMES == 0:
+    sqls.append(
+    "CREATE TABLE {}.{} AS "
+    "SELECT "
+    "l.pfafid_30spfaf06, "
+    "l.temporal_resolution, "
+    "l.year, "
+    "l.month, "
+    "l.area_m2_30spfaf06, "
+    "l.area_count_30spfaf06, "
+    "l.ma10_pdomww_m_30spfaf06, "
+    "l.ma10_pindww_m_30spfaf06, "
+    "l.ma10_pirrww_m_30spfaf06, "
+    "l.ma10_plivww_m_30spfaf06, "
+    "l.ma10_ptotww_m_30spfaf06, "
+    "l.ma10_pdomwn_m_30spfaf06, "
+    "l.ma10_pindwn_m_30spfaf06, "
+    "l.ma10_pirrwn_m_30spfaf06, "
+    "l.ma10_plivwn_m_30spfaf06, "
+    "l.ma10_ptotwn_m_30spfaf06, "
+    "l.ma10_riverdischarge_m_30spfaf06, "
+    "l.arid_boolean_30spfaf06, "
+    "l.lowwateruse_boolean_30spfaf06, "
+    "l.aridandlowwateruse_boolean_30spfaf06, "
+    "l.waterstress_dimensionless_30spfaf06, "
+    "r.pfaf_id, "
+    "r.coast, "
+    "r.geom "
+    "FROM {}.temp_left AS l "
+    "INNER JOIN {} AS r ON "
+    "r.pfaf_id = l.pfafid_30spfaf06 ".format(OUTPUT_SCHEMA,OUTPUT_TABLE_NAME,OUTPUT_SCHEMA,INPUT_TABLE_RIGHT_NAME))
+elif SIMPLIFY_COLUMN_NAMES == 1:
+    sqls.append(
+    "CREATE TABLE {}.{} AS "
+    "SELECT "
+    "l.pfafid_30spfaf06 AS pfafid, "
+    "l.temporal_resolution AS temp_res, "
+    "l.year, "
+    "l.month, "
+    "l.area_m2_30spfaf06 AS area_m2, "
+    "l.area_count_30spfaf06 AS area_count, "
+    "l.ma10_pdomww_m_30spfaf06 AS pdomww_m, "
+    "l.ma10_pindww_m_30spfaf06 AS pindww_m, "
+    "l.ma10_pirrww_m_30spfaf06 AS pirrww_m, "
+    "l.ma10_plivww_m_30spfaf06 AS plivww_m, "
+    "l.ma10_ptotww_m_30spfaf06 AS ptotww_m, "
+    "l.ma10_pdomwn_m_30spfaf06 AS pdomwn_m, "
+    "l.ma10_pindwn_m_30spfaf06 AS pindwn_m, "
+    "l.ma10_pirrwn_m_30spfaf06 AS pirrwn_m, "
+    "l.ma10_plivwn_m_30spfaf06 AS plivwn_m, "
+    "l.ma10_ptotwn_m_30spfaf06 AS ptotwn_m, "
+    "l.ma10_riverdischarge_m_30spfaf06 AS q_m, "
+    "l.arid_boolean_30spfaf06 AS arid, "
+    "l.lowwateruse_boolean_30spfaf06 AS lowwatuse, "
+    "l.aridandlowwateruse_boolean_30spfaf06 AS aridlow, "
+    "l.waterstress_dimensionless_30spfaf06 AS ws_s, "
+    "r.pfaf_id, "
+    "r.coast, "
+    "r.geom "
+    "FROM {}.temp_left AS l "
+    "INNER JOIN {} AS r ON "
+    "r.pfaf_id = l.pfafid_30spfaf06 ".format(OUTPUT_SCHEMA,OUTPUT_TABLE_NAME,OUTPUT_SCHEMA,INPUT_TABLE_RIGHT_NAME))   
+    
+    
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[7]:
 
 sqls.append(
-"CREATE TABLE {}.temp_left AS "
-"SELECT * FROM {} "
-"WHERE "
-"pfafid_30spfaf06 > {} AND "
-"pfafid_30spfaf06 < {} AND "
-"temporal_resolution = '{}' AND "
-"year = {} AND "
-"month = {};".format(OUTPUT_SCHEMA ,INPUT_TABLE_LEFT_NAME,PFAFID_RANGE_MIN,PFAFID_RANGE_MAX,TEMPORAL_RESOLUTION,YEAR,MONTH))
-
-
-# In[ ]:
-
-# old sqls
-sqls.append(
-"CREATE TABLE {}.{} AS "   
-"SELECT "
-"hybas06_v04.pfaf_id, "
-"hybas06_v04.geom, "
-"month, "
-"year, "
-"area_m2_30spfaf06, "
-"area_count_30spfaf06, "
-"ma10_pdomww_m_30spfaf06, "
-"ma10_pindww_m_30spfaf06, "
-"ma10_pirrww_m_30spfaf06, "
-"ma10_plivww_m_30spfaf06, "
-"ma10_ptotww_m_30spfaf06, "
-"ma10_pdomwn_m_30spfaf06, "
-"ma10_pindwn_m_30spfaf06, "
-"ma10_pirrwn_m_30spfaf06, "
-"ma10_plivwn_m_30spfaf06, "
-"ma10_ptotwn_m_30spfaf06, "
-"ma10_riverdischarge_m_30spfaf06, "
-"arid_boolean_30spfaf06, "
-"lowwateruse_boolean_30spfaf06, "
-"aridandlowwateruse_boolean_30spfaf06, "
-"waterstress_dimensionless_30spfaf06, "
-"pfafid_30spfaf06 "
-"FROM hybas06_v04 "
-"INNER JOIN test.temp_right ON test.temp_right.pfafid_30spfaf06 = hybas06_v04.pfaf_id".format(OUTPUT_SCHEMA,OUTPUT_TABLE_NAME)
-)
-
-
-# In[ ]:
-
-SELEC
-
-
-
+"ALTER TABLE {}.{} ADD id BIGSERIAL PRIMARY KEY;".format(OUTPUT_SCHEMA,OUTPUT_TABLE_NAME))
 
 
 # In[ ]:
@@ -143,12 +205,12 @@ SELEC
 
 
 
-# In[13]:
+# In[8]:
 
 sqls
 
 
-# In[6]:
+# In[ ]:
 
 for sql in sqls:
     print(sql)
@@ -164,7 +226,9 @@ print(elapsed)
 
 # Previous runs:  
 # 0:00:03.647104  
-# 0:01:37.677627
+# 0:01:37.677627  
+# 0:05:18.544848
+# 
 # 
 # 
 # 
